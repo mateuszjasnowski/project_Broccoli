63061380dd5c23b97fa2a8478a585538d5a03df1
Mateusz Jasnowski <mateuszjasnowski99@gmail.com>
2022-01-18 22:57:39 +0100

    Add README, Post archive, and minor changes

--- appData/routes.py
+++ appData/routes.py
@@ -3,9 +3,8 @@ import secrets, os
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from appData import app, db, bcrypt
-#from appData.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from appData.models import User, Post
-from flask_login import login_user, current_user, logout_user, login_required #copy
+from flask_login import login_user, current_user, logout_user, login_required
 
 
 class BroccoliRegisterForm:
@@ -16,7 +15,6 @@ class BroccoliRegisterForm:
         self.email = email
         self.password = password
 
-
     def isUsernameUsed(self):
         user = User.query.filter_by(login=self.username).first()
         if user:
@@ -24,13 +22,13 @@ class BroccoliRegisterForm:
         else:
             return True
 
-
     def isEmailUsed(self):
         email = User.query.filter_by(email=self.email).first()
         if email:
             return False
         else:
             return True
+
 
 class BroccoliLoginForm:
     def __init__(self, username, password, rememberMe):
@@ -47,7 +45,8 @@ class BroccoliLoginForm:
 
 
 class BroccoliNewPostForm:
-    def __init__(self, manufacture, model, manufacture_year, price, photo, description, author, location): #author = user_id
+    def __init__(self, manufacture, model, manufacture_year, price, photo, description, author,
+                 location):  # author = user_id
         self.manufacture = manufacture
         self.model = model
         self.manufacture_year = manufacture_year
@@ -56,7 +55,8 @@ class BroccoliNewPostForm:
         self.description = description
         self.author = author
         self.location = location
-    #TODO form validation
+    # TODO form validation
+
 
 class BroccoliStatistics:
     def __init__(self, sumOfPosts, sumOfPostsToday, sumOfUsers, buildNumber):
@@ -65,12 +65,13 @@ class BroccoliStatistics:
         self.sumOfUsers = sumOfUsers
         self.buildNumber = buildNumber
 
-#side routes
+
+# side routes
 @app.route('/')
 @app.route('/home')
 def home():
     posts = Post.query.all()
-    return render_template('index.html', title='Start', posts = posts)
+    return render_template('index.html', title='Start', posts=posts)
 
 
 @app.route('/login')
@@ -81,9 +82,11 @@ def login():
         return redirect(url_for('home'))
     return render_template('login.html', title='Logowanie', next_page=next_page)
 
-@app.route('/login_proceed', methods=["POST","GET"])
+
+@app.route('/login_proceed', methods=["POST", "GET"])
 def login_proceed():
-    formData = BroccoliLoginForm(request.form.get('login'),request.form.get('password'),request.form.get('remember-me'))
+    formData = BroccoliLoginForm(request.form.get('login'), request.form.get('password'),
+                                 request.form.get('remember-me'))
     if formData.isUsernameUsed():
         user = User.query.filter_by(login=formData.username).first()
         if user.role == "Unverified":
@@ -101,11 +104,10 @@ def login_proceed():
                 else:
                     return redirect(url_for('home'))
             else:
-                #flash(user.password,'danger')
                 flash('Błędny login lub hasło.', 'danger')
                 return redirect(url_for('login'))
     flash('Konto o podanym loginie nie istnieje. Spróbuj inny login lub zarejestruj konto', 'danger')
-    return redirect(url_for('login')) #TODO reset password
+    return redirect(url_for('login'))  # TODO reset password
 
 
 @app.route("/logout")
@@ -120,9 +122,12 @@ def register():
         return redirect(url_for('home'))
     return render_template('register.html', title='Rejestracja')
 
+
 @app.route('/register_proceed', methods=["POST"])
 def register_proceed():
-    formData = BroccoliRegisterForm(request.form.get('login'),request.form.get('userFirstName'),request.form.get('userLastName'),request.form.get('email'),request.form.get('password'))
+    formData = BroccoliRegisterForm(request.form.get('login'), request.form.get('userFirstName'),
+                                    request.form.get('userLastName'), request.form.get('email'),
+                                    request.form.get('password'))
     if formData.isUsernameUsed():
         if formData.isEmailUsed():
             hashed_password = bcrypt.generate_password_hash(formData.password).decode('utf-8')
@@ -132,21 +137,21 @@ def register_proceed():
             flash('Konto zostało utworzone! Możesz się teraz zalogować.', 'success')
             return redirect(url_for('login'))
         else:
-            flash('Istnieje już konto z podanym adresem email.','danger')
+            flash('Istnieje już konto z podanym adresem email.', 'danger')
             return redirect(url_for('login'))
     else:
-        flash('Użytkownik o podanym loginie już istnieje. Wybierz inny login.','danger')
+        flash('Użytkownik o podanym loginie już istnieje. Wybierz inny login.', 'danger')
         return redirect(url_for('register'))
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html',title='O projekcie') #TODO
+    return render_template('about.html', title='O projekcie')  # TODO
 
 
 @app.route('/terms')
 def terms():
-    return 'Terms' #TODO
+    return 'Terms'  # TODO
 
 
 @app.route('/post/new_post')
@@ -162,27 +167,35 @@ def save_picture(form_picture):
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/photos', picture_fn)
 
-    output_size = (850, 450) #maximum image size on page
+    output_size = (850, 450)  # maximum image size on page
     i = Image.open(form_picture)
     i.thumbnail(output_size)
     i.save(picture_path)
 
     return picture_fn
 
+
 @app.route('/post/new_post_publish', methods=["POST"])
 @login_required
 def new_post_publish():
-    formData = BroccoliNewPostForm(request.form.get('manufacture'),request.form.get('model'),request.form.get('manufacture_year'),request.form.get('price'),request.files['photo'],request.form.get('description'),current_user.id,request.form.get('location'))
-    #TODO form validation placeholder
+    formData = BroccoliNewPostForm(request.form.get('manufacture'), request.form.get('model'),
+                                   request.form.get('manufacture_year'), request.form.get('price'),
+                                   request.files['photo'], request.form.get('description'), current_user.id,
+                                   request.form.get('location'))
+    # TODO form validation placeholder
     if formData.photo:
         carPhotoFile = request.files['photo']
         carPhoto = save_picture(carPhotoFile)
-        post = Post(user_id=formData.author, manufacture=formData.manufacture, model=formData.model, manufacture_year=formData.manufacture_year, photo=carPhoto, description=formData.description, price=formData.price, location=formData.location)
+        post = Post(user_id=formData.author, manufacture=formData.manufacture, model=formData.model,
+                    manufacture_year=formData.manufacture_year, photo=carPhoto, description=formData.description,
+                    price=formData.price, location=formData.location)
     else:
-        post = Post(user_id=formData.author, manufacture=formData.manufacture, model=formData.model, manufacture_year=formData.manufacture_year, description=formData.description, price=formData.price, location=formData.location)
+        post = Post(user_id=formData.author, manufacture=formData.manufacture, model=formData.model,
+                    manufacture_year=formData.manufacture_year, description=formData.description, price=formData.price,
+                    location=formData.location)
     db.session.add(post)
     db.session.commit()
-    flash('Utworzono nowe głoszenie !','success')
+    flash('Utworzono nowe głoszenie !', 'success')
     return redirect(url_for('home'))
 
 
@@ -191,12 +204,13 @@ def post(post_id):
     post = Post.query.get_or_404(post_id)
     title = 'Ogłoszenie - ' + post.manufacture + ' ' + post.model
     if post.author.role == "Blocked" or post.status == 'Blocked':
-        flash('Ogłoszenie jest niedostępne','warning')
+        flash('Ogłoszenie jest niedostępne', 'warning')
         if current_user.role != "Admin":
             return redirect(url_for('home'))
     if post.status == 'Archived':
-        flash('Ogłoszenie nie jest już aktualne','info')
+        flash('Ogłoszenie nie jest już aktualne', 'info')
     return render_template('post.html', post=post, title=title)
+
 
 @app.route('/post/<int:post_id>/edit')
 @login_required
@@ -205,17 +219,18 @@ def edit_post(post_id):
     thisYear = int(datetime.now().strftime("%Y"))
     title = 'Edytuj ogłoszenie - ' + post.manufacture + ' ' + post.model
     if current_user.id == post.author.id or current_user.role == 'Admin':
-        statuses = ['Published','Archived','Blocked']
-        return render_template('edit_post.html', post=post,  current_year=thisYear, title=title, statuses=statuses)
+        statuses = ['Published', 'Archived', 'Blocked']
+        return render_template('edit_post.html', post=post, current_year=thisYear, title=title, statuses=statuses)
     else:
-        flash('Nie możesz edytować ogłoszenia które nie jest twoje!','warning')
-        redirectLocation = '/post/'+str(post.id)
+        flash('Nie możesz edytować ogłoszenia które nie jest twoje!', 'warning')
+        redirectLocation = '/post/' + str(post.id)
         return redirect(redirectLocation)
+
 
 @app.route('/post/<int:post_id>/edit_proceed', methods=["POST"])
 @login_required
 def edit_proceed(post_id):
-    formData = BroccoliNewPostForm(request.form.get('manufacture'),request.form.get('model'),request.form.get('manufacture_year'),request.form.get('price'),request.files['photo'],request.form.get('description'),current_user.id,request.form.get('location'))
+    formData = BroccoliNewPostForm(request.form.get('manufacture'), request.form.get('model'), request.form.get('manufacture_year'), request.form.get('price'), request.files['photo'], request.form.get('description'), current_user.id, request.form.get('location'))
     post = Post.query.get_or_404(post_id)
     post.manufacture = formData.manufacture
     post.model = formData.model
@@ -230,8 +245,9 @@ def edit_proceed(post_id):
     if request.form.get('status'):
         post.status = request.form.get('status')
     db.session.commit()
-    redirectLocation = '/post/'+str(post.id)
+    redirectLocation = '/post/' + str(post.id)
     return redirect(redirectLocation)
+
 
 @app.route('/post/<int:post_id>/delete')
 @login_required
@@ -240,16 +256,16 @@ def delete_post(post_id):
     if current_user.id == post.author.id or current_user.role == 'Admin':
         if post.photo != 'defaultCar.jpg':
             os.remove(os.path.join(app.root_path, 'static/photos', post.photo))
-        db.session.query(Post).filter(Post.id==post.id).delete()
+        db.session.query(Post).filter(Post.id == post.id).delete()
         db.session.commit()
-        flash('Ogłoszenie zostało usunięte','success')
+        flash('Ogłoszenie zostało usunięte', 'success')
         if request.args.get('back'):
             return redirect(url_for(request.args.get('back')))
         else:
             return redirect(url_for('home'))
     else:
-        flash('Nie możesz wykonać tej akcji','danger')
-        redirectLocation = '/post/'+str(post.id)
+        flash('Nie możesz wykonać tej akcji', 'danger')
+        redirectLocation = '/post/' + str(post.id)
         return redirect(redirectLocation)
 
 
@@ -258,11 +274,11 @@ def delete_post(post_id):
 def user(user_id):
     user = User.query.get_or_404(user_id)
     postOfUser = Post.query.filter_by(user_id=user.id).all()
-    title = 'Użytkownik '+user.login
+    title = 'Użytkownik ' + user.login
     return render_template('user.html', user=user, title=title, posts=postOfUser)
 
 
-def userPasswordChnage(user_id,oldPassword,newPassword):
+def userPasswordChnage(user_id, oldPassword, newPassword):
     return 'Hi'
 
 
@@ -271,12 +287,12 @@ def userPasswordChnage(user_id,oldPassword,newPassword):
 def edit_user(user_id):
     user = User.query.get_or_404(user_id)
     title = 'Edytuj konto ' + user.login
-    roles = ['User','Admin','Unverified','Blocked']
+    roles = ['User', 'Admin', 'Unverified', 'Blocked']
     if current_user.id == user.id or current_user.role == 'Admin':
         return render_template('edit_user.html', user=user, title=title, roles=roles)
     else:
-        flash('Nie możesz edytować tego konta!','warning')
-        redirectLocation = '/user/'+str(user.id)
+        flash('Nie możesz edytować tego konta!', 'warning')
+        redirectLocation = '/user/' + str(user.id)
         return redirect(redirectLocation)
 
 
@@ -296,24 +312,25 @@ def user_proceed(user_id):
         return 'change password'
     else:
         db.session.commit()
-        redirectLocation = '/user/'+str(user.id)
+        redirectLocation = '/user/' + str(user.id)
         return redirect(redirectLocation)
+
 
 @app.route('/user/<int:user_id>/delete')
 @login_required
 def delete_user(user_id):
     user = User.query.get_or_404(user_id)
     if current_user.id == user.id or current_user.role == 'Admin':
-        db.session.query(User).filter(User.id==user.id).delete()
+        db.session.query(User).filter(User.id == user.id).delete()
         db.session.commit()
-        flash('Konto zostało usunięte','success')
+        flash('Konto zostało usunięte', 'success')
         if request.args.get('back'):
             return redirect(url_for(request.args.get('back')))
         else:
             return redirect(url_for('home'))
     else:
-        flash('Nie możesz wykonać tej akcji','danger')
-        redirectLocation = '/user/'+str(user.id)
+        flash('Nie możesz wykonać tej akcji', 'danger')
+        redirectLocation = '/user/' + str(user.id)
         return redirect(redirectLocation)
 
 
@@ -323,32 +340,53 @@ def admin_panel():
     if current_user.role == 'Admin':
         users = User.query.all()
         posts = Post.query.all()
-        statistics = BroccoliStatistics(len(posts),len(posts),len(users),'0.21.2') #TODO separate posts created today #BUILD NUM HERE
-        return render_template('admin_panel.html', title='Panel administratora', users=users, posts=posts, statistics=statistics)
+        statistics = BroccoliStatistics(len(posts), len(posts), len(users),
+                                        '0.21.3')  # TODO separate posts created today #BUILD NUM HERE
+        return render_template('admin_panel.html', title='Panel administratora', users=users, posts=posts,
+                               statistics=statistics)
     else:
-        flash('Nie jesteś administratorem!','warning')
+        flash('Nie jesteś administratorem!', 'warning')
         return redirect(url_for('home'))
+
 
 @app.route('/post/<int:post_id>/delete/action_confirm', methods=["GET"])
 @login_required
 def post_delete_action_confirm(post_id):
     post = Post.query.get_or_404(post_id)
-    nextAction='/post/'+str(post.id)+'/delete'
+    nextAction = '/post/' + str(post.id) + '/delete'
     previousAction = request.args.get('back')
     if current_user.id == post.author.id or current_user.role == 'Admin':
-        return render_template('action_confirm.html',nextAction=nextAction,previousAction=previousAction,title='Potwierdź usunięcie ogłoszenia')
+        return render_template('action_confirm.html', nextAction=nextAction, previousAction=previousAction,
+                               title='Potwierdź usunięcie ogłoszenia')
     else:
-        flash('Nie możesz wykonać tej czynności!','warning')
+        flash('Nie możesz wykonać tej czynności!', 'warning')
         return redirect(url_for(previousAction))
+
 
 @app.route('/user/<int:user_id>/delete/action_confirm', methods=["GET"])
 @login_required
 def user_delete_action_confirm(user_id):
     user = User.query.get_or_404(user_id)
-    nextAction='/user/'+str(user.id)+'/delete'
+    nextAction = '/user/' + str(user.id) + '/delete'
     previousAction = request.args.get('back')
     if current_user.id == user.id or current_user.role == 'Admin':
-        return render_template('action_confirm.html',nextAction=nextAction,previousAction=previousAction,title='Potwierdź usunięcie urzytkownika')
+        return render_template('action_confirm.html', nextAction=nextAction, previousAction=previousAction,
+                               title='Potwierdź usunięcie urzytkownika')
     else:
-        flash('Nie możesz wykonać tej czynności!','warning')
+        flash('Nie możesz wykonać tej czynności!', 'warning')
         return redirect(url_for(previousAction))
+
+
+@app.route('/post/<int:post_id>/archive')
+@login_required
+def post_archive(post_id):
+    post = Post.query.get_or_404(post_id)
+    if current_user.id == post.author.id or current_user.role == 'Admin':
+        post.status = 'Archived'
+        db.session.commit()
+        flash('Ogłoszenie zostało zarchiwizowane','success')
+        return redirect(url_for('home'))
+    else:
+        flash('Nie masz dostępu do tej opcji','warning')
+        rediectLocation = '/post/'+str(post.id)
+        return redirect(rediectLocation)
