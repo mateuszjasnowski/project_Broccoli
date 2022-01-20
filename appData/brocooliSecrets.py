a7dfe7e05b28824b15b96f5c756a9f65390efeb4
Ja-Sny <matiman34@gmail.com>
2022-01-20 12:51:08 +0100

    beta commit

---
+++ appData/brocooliSecrets.py
@@ -0,0 +1,5 @@
+appIp = '127.0.0.1'
+appVersion = 'v0.1.1-beta'
+
+emailAddress = "You won't get it from me"
+emailSecret = "You won't get it from me"
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os,webbrowser
 from appData import app
+from appData.brocooliSecrets import appIp
 
 
 def main():
     #if not os.environ.get("WERKZEUG_RUN_MAIN"):
     #    webbrowser.open_new('http://127.0.0.1:2000/')
-    app.run(host='127.0.0.1', port=2000, debug=True)
+    app.run(host=appIp, port=2000, debug=True)
 
 
 if __name__ == '__main__':
