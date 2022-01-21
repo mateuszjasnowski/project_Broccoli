abb7a5d36036283a2382e3534a6f890c7febd5a7
Mateusz Jasnowski <matiman34@gmail.com>
2022-01-21 12:45:20 +0100

    ready to use mysql server

--- appData/brocooliSecrets.py
+++ appData/brocooliSecrets.py
@@ -1,10 +1,10 @@
 appIp = '127.0.0.1' #'project-broccoli.ew.r.appspot.com'
-appVersion = 'v0.1.3.1-beta'
+appVersion = 'v0.1.3.2-beta'
 
-emailAddress = "none"
-emailSecret = "none"
+emailAddress = "None"
+emailSecret = "None"
 
-dbAddress = 'none'
-dbUser = 'none'
-dbSecret = 'none'
-dbDATABASE_URI = 'mysql+pymysql://'+dbUser+":"+dbSecret+'@'+dbAddress+'/broccoli'
+dbAddress = "None"
+dbUser = "None"
+dbSecret = "None"
+dbDATABASE_URI = 'mysql+pymysql://'+dbUser+":"+dbSecret+'@'+dbAddress+'/projectborccoli'
